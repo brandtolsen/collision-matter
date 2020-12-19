d18a10adea3fd9ca322e69d685d7dfc14f541d78
Daniel Brandt-Olsen <daniel@brandtolsen.com>
2020-12-19 17:02:32 +0100

    adding more options from Matter

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // alias is a shortcut to make the code cleaner
 // const Engine = Matter.Engine
 // const Render = Matter.Render
-const {Engine, Render, Bodies, World} = Matter
+const {Engine, Render, Bodies, World, MouseConstraint, Composites} = Matter
 
 // where is matter being deployed
 const sectionTag = document.querySelector("section.shapes")
@@ -38,11 +38,44 @@ const createShape = function (x, y) {
 const bigBall = Bodies.circle(w / 2, h / 2, 250, {
     isStatic: true,
     render: {
-        fillStyle: "#000"
+        fillStyle: "#1E1F41"
     }
 })
 
-World.add(engine.world, bigBall)
+const wallOptions = {
+    isStatic: true,
+    render: {
+        visible: false
+    }
+}
+
+const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 120, wallOptions)
+const ceiling = Bodies.rectangle(w / 2, -50, w + 100, 120, wallOptions)
+const leftWall = Bodies.rectangle(-50, h / 2, 100, h + 100, wallOptions)
+const rightWall = Bodies.rectangle(w + 50, h / 2, 100, h + 100, wallOptions)
+
+const mouseControl = MouseConstraint.create(engine, {
+    element: sectionTag,
+    constraint: {
+        render: {
+            visible: false
+        }
+    }
+})
+
+const initialShapes = Composites.stack(50, 50, 15, 5, 40, 40, function (x, y) {
+    return createShape(x, y)
+})
+
+World.add(engine.world, [
+    bigBall, 
+    ground,
+    ceiling,
+    leftWall,
+    rightWall,
+    mouseControl,
+    initialShapes
+])
 
 // when we click the page, add a new shape
 document.addEventListener("click", function (event) {

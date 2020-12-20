3260c2232c2b58843a4d1631889e13d73adf0bd3
Daniel Brandt-Olsen <daniel@brandtolsen.com>
2020-12-20 12:46:38 +0100

    added resize event

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const renderer = Render.create({
 
 // create vector shapes
 const boxA = function (x, y) {
-    return Bodies.rectangle(x, y, Math.min(w/8, h/8), Math.min(w/8, h/8), {
+    return Bodies.rectangle(x, y, Math.min(w/10, h/10), Math.min(w/10, h/10), {
         frictionAir: 0,
         friction: 0,
         frictionStatic: 1,
@@ -32,7 +32,7 @@ const boxA = function (x, y) {
     })
 }
 const boxB = function (x, y) {
-    return Bodies.rectangle(x, y, Math.min(w/6, h/6), Math.min(w/6, h/6), {
+    return Bodies.rectangle(x, y, Math.min(w/8, h/8), Math.min(w/8, h/8), {
         frictionAir: 0,
         friction: 0,
         frictionStatic: 1,
@@ -49,7 +49,7 @@ const boxB = function (x, y) {
 const createShape = function (x, y) {
     const randomNum = Math.random()
 
-    if (randomNum > 0.8) {
+    if (randomNum > 0.7) {
         return Bodies.rectangle(x, y, 75, 62.25, {
             render: {
                 sprite: {
@@ -63,6 +63,11 @@ const createShape = function (x, y) {
     
     else if (randomNum > 0.5) { 
         return Bodies.circle(x, y, 50, {
+            frictionAir: 0,
+            friction: 0,
+            frictionStatic: 1,
+            inertia: Infinity,
+            restitution: 1,
             render: {
                 sprite: {
                     texture: "splitcircle-2x.png",
@@ -86,7 +91,7 @@ const createShape = function (x, y) {
     }
 }
 
-const bigBall = Bodies.rectangle(w / 2, h / 2, Math.min(w/1.5, h/1.5), 110, {
+const transparentSquare = Bodies.rectangle(w / 2, h / 1.95, Math.min(w/1.9, h/1.9), Math.min(w/10, h/10), {
     isStatic: true,
     render: {
         fillStyle: "#fff"
@@ -114,7 +119,7 @@ const mouseControl = MouseConstraint.create(engine, {
     }
 })
 
-const initialShapes = Composites.stack(50, 50, 15, 5, 40, 40, function (x, y) {
+const initialShapes = Composites.stack(50, 50, 15, 4, 40, 40, function (x, y) {
     return createShape (x, y)
 })
 const box1 = Composites.stack(50, 50, 10, 1, 40, 40, function (x, y) {
@@ -125,7 +130,7 @@ const box2 = Composites.stack(50, 50, 10, 1, 40, 40, function (x, y) {
 })
 
 World.add(engine.world, [
-    bigBall, 
+    transparentSquare, 
     ground,
     ceiling,
     leftWall,
@@ -144,3 +149,18 @@ window.addEventListener("deviceorientation", function (event) {
     engine.world.gravity.x = event.gamma / 30
     engine.world.gravity.y = event.beta / 30
 })
+
+window.addEventListener("resize", function () {
+    w = window.innerWidth
+    h = window.innerHeight
+    renderer.canvas.width = w
+    renderer.canvas.height = h
+    renderer.canvas.style.width = w + "px"
+    renderer.canvas.style.height = h + "px" 
+    renderer.options.width = w
+    renderer.options.height = h
+    
+    Matter.Body.setStatic(transparentSquare, false)
+    transparentSquare.position =  { x: w / 2, y: h / 2 }
+    Matter.Body.setStatic(transparentSquare, true)
+  })
